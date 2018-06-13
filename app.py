300ce447f32261543468681510c44768f5a9ab44
I-Chun Liu <arthur801031@gmail.com>
2018-06-13 14:16:16 +0800

    Finished lesson 139

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ import os
 
 from flask import Flask
 from flask_restful import Api
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
 
-from security import authenticate, identity
-from resources.user import UserRegister
+from resources.user import UserRegister, User, UserLogin
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -15,17 +14,25 @@ app.config['DEBUG'] = True
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'jose'
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.secret_key = 'jose' # app.config['JWT_SECRET_KEY']
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity)  # /auth
+jwt = JWTManager(app)  # not creating /auth
+
+@jwt.user_claims_loader
+def add_claims_to_jwt(identity):
+    if identity == 1: # Instead of hard-coding, you should read from a config file or a database
+        return {'is_admin': True}
+    return {'is_admin': False}
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
-
 api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
 
 if __name__ == '__main__':
     from db import db
